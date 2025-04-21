95ced66a3d877e01c1f42197dfbc5bb79ef1464f
maria <konovalova132reserve@gmail.com>
2025-04-21 22:40:04 +0300

    Update task1_time_fixed.c

--- task1_time_fixed.c
+++ task1_time_fixed.c
@@ -6,6 +6,6 @@ int main(){
     time_t t_max = (time_t)LONG_MAX;
     printf ("max val of time_t -> %ld \n", (long)t_max); 
     printf ("time_t size -> %lu bytes \n", sizeof (time_t)); 
-    printf ("system bit rate -> bit \n", sizeof (void *)*8);
+    printf ("system bit rate -> %lu bit \n", sizeof (void *)*8);
     return 0;
 }
