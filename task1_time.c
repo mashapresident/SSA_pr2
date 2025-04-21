15414a464f3cfef62a7a47646741d491f6ad725e
maria <konovalova132reserve@gmail.com>
2025-04-21 22:39:37 +0300

    Update task1_time.c

--- task1_time.c
+++ task1_time.c
@@ -6,7 +6,7 @@ int main(){
     time_t time = 0;
     while(time >= 0) time++;
     printf ("max value of time_t -> %ld /n", (long)time); 
-    printf ("represent -> ¼s /n", ctime (&time)); 
+    printf ("represent -> %s /n", ctime (&time)); 
     printf ("time_t size -> %lu bytes /n", sizeof (time_t)); 
     printf ("system bit rate -› ", sizeof (void *)*8);
     return 0;
